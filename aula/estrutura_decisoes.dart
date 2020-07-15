dd93c179a26096f1706eede16da3e20062bcf060
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-07-15 16:18:31 -0300

    Estrutura de decisoes

---
+++ aula/estrutura_decisoes.dart
@@ -0,0 +1,26 @@
+main() {
+  var num1 = 4;
+  var num2 = num1 + 1;
+
+//Operadores usado para comparacoes
+// != diferença
+// == igualdade
+// > maior que
+// < menor que
+// >= maior ou igual
+// <= menor ou igual
+  var isTrue = num1 != num2;
+
+  // isTrue = num1 != num2;
+  // isTrue = num1 == num2;
+  // isTrue = num1 > num2;
+  // isTrue = num1 < num2;
+  // isTrue = num1 >= num2;
+  // isTrue = num1 <= num2;
+
+  if (isTrue) {
+    print("É verdadeiro");
+  } else {
+    print("É Falso");
+  }
+}
