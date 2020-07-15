a9045fff6f08c7cb8d923ef0b84a08f1a7121f6c
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-07-15 15:52:01 -0300

    Operaçoes matematica

---
+++ aula/operacoes_matematica.dart
@@ -0,0 +1,22 @@
+main() {
+  //Adição
+  var ad = 3 + 5;
+
+  //Subtração
+  var sub = 5 - 3;
+
+  //Multiplicação
+  var mut = 5 * 3;
+
+  //Divisão
+  var div = 8 / 2;
+
+  //Resto
+  var res = 15 % 3;
+
+  print(ad);
+  print(sub);
+  print(mut);
+  print(div);
+  print(res);
+}
